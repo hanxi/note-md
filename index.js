f82a6b51de6c2b7835d56179a85063ef1514cfeb
hanxi <hanxi.info@gmail.com>
2017-01-05 12:32:23 +0800

    全部改为异步接口

--- index.js
+++ index.js
@@ -23,22 +23,36 @@ const mimeTypes = {
     'css': 'text/css'
 };
 
+
+const asyncExistsFile = (filename) => {
+    return new Promise(function(resolve, reject) {
+        fs.exists(filename, (exists) => {
+            resolve([filename, exists]);
+        })
+    });
+}
 const staticFilePaths = [process.cwd(), noteDir];
 const existsStaticFile = (pathname, callback) => {
+    let promiseArr = [];
     for (let parrentPath of staticFilePaths) {
         let filename = path.join(parrentPath, pathname);
-        if (fs.existsSync(filename)) {
-            return callback(filename);
-        }
+        promiseArr.push(asyncExistsFile(filename));
     }
-    callback(null);
+
+    Promise.all(promiseArr).then((results) => {
+        for (let result of results) {
+            if (result[1]) {
+                return callback(result[0]);
+            }
+        }
+        callback(null);
+    });
 };
 
 const generatePaths = (pathname) => {
     let pathList = pathname.split('/');
     for (let i=0; i<pathList.length-1; i++) {
     }
-    console.log('pathList:', pathList);
     let paths = [{
         'path': '/',
         'name': 'index'
@@ -47,7 +61,6 @@ const generatePaths = (pathname) => {
     for (let i=0; i<pathList.length; i++) {
         let p = pathList[i];
         if (p!=='') {
-            console.log('path:', p);
             pathPre += p;
             if (i!==pathList.length-1) {
                 pathPre += '/';
@@ -58,49 +71,87 @@ const generatePaths = (pathname) => {
             });
         }
     }
-    console.log(paths)
     return paths;
 };
 
-const generateNotes = (pathname, dirname) => {
-    let files = fs.readdirSync(dirname);
-    let notes = [];
-    for (let p of files) {
-        console.log('xxxxx', path.extname(p));
-        let pname = path.join(dirname, p);
-        let stat = fs.statSync(pname);
-        if (stat.isDirectory()) {
-            notes.push({
-                'title': p,
-                'path': pathname + p + '/'
-            });
-        } else if (stat.isFile()) {
-            let ext = path.extname(p).split('.')[1];
-            if (ext=='md') {
+const asyncGetNotes = (notes, pathname, dirname, p) => {
+    let pname = path.join(dirname, p);
+    return new Promise(function(resolve, reject) {
+        fs.stat(pname, (err, stat) => {
+            if (stat.isDirectory()) {
                 notes.push({
                     'title': p,
-                    'path': pathname + p
+                    'path': pathname + p +'/'
                 });
+            } else if (stat.isFile()) {
+                if (p.substr(-3)==='.md') {
+                    notes.push({
+                        'title': p,
+                        'path': pathname + p
+                    });
+                }
             }
-        }
-    }
-    console.log(notes);
-    return notes;
+            resolve();
+        });
+    });
 };
 
-const notFound = (res) => {
+const generateNotes = (pathname, dirname, callback) => {
+    fs.readdir(dirname, (err, files) => {
+        if (err) {
+            console.error(err);
+            return callback([]);
+        }
+        let notes = [];
+        let promiseArr = [];
+        for (let p of files) {
+            promiseArr.push(asyncGetNotes(notes, pathname, dirname, p));
+        }
+        Promise.all(promiseArr).then((results) => {
+            callback(notes);
+        });
+    });
+};
+
+const sendDirectoryRender = (res, pathname, dirname) => {
+    let paths = generatePaths(pathname);
+    generateNotes(pathname, dirname, (notes) => {
+        let data = {
+            'paths': paths,
+            'notes': notes
+        };
+        res.end(directoryTemplate(data));
+    });
+};
+
+const notFound = (req, res) => {
+    console.error(req.url, ' Not Found');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('404 Not Found\n');
     res.end();
     return;
 };
 
-const parseNote = (filename) => {
-    let text = fs.readFileSync(filename, 'utf8');
-    let html = converter.makeHtml(text);
-    return html;
+const parseNote = (filename, callback) => {
+    fs.readFile(filename, 'utf8', (err, text) => {
+        if (err) {
+            text = err.message;
+        }
+        let html = converter.makeHtml(text);
+        callback(html);
+    });
 };
 
+const sendNoteRender = (res, pathname, filename) => {
+    let paths = generatePaths(pathname);
+    parseNote(filename, (note) => {
+        let data = {
+            'paths': paths,
+            'note': note
+        };
+        res.end(noteTemplate(data));
+    });
+};
 
 const requestHandler = (req, res) => {
     let pathname = urlParse(req.url).pathname;
@@ -109,7 +160,7 @@ const requestHandler = (req, res) => {
     if (mimeType) {
         existsStaticFile(pathname, (filename) => {
             if (!filename) {
-                return notFound(res);
+                return notFound(req, res);
             }
             res.writeHead(200, mimeType);
             let fileStream = fs.createReadStream(filename);
@@ -117,32 +168,23 @@ const requestHandler = (req, res) => {
         });
     } else {
         if (pathname.substr(-1)==='/') {
-            console.log('in directory');
             let dirname = path.join(noteDir, pathname);
             fs.exists(dirname, (exists) => {
                 if (!exists) {
-                    return notFound(res);
+                    return notFound(req, res);
                 }
-                let data = {
-                    'paths': generatePaths(pathname),
-                    'notes': generateNotes(pathname, dirname)
-                };
-                res.end(directoryTemplate(data));
+                sendDirectoryRender(res, pathname, dirname);
             });
         } else if (pathname.substr(-3)==='.md') {
             let filename = path.join(noteDir, pathname);
             fs.exists(filename, (exists) => {
                 if (!exists) {
-                    return notFound(res);
+                    return notFound(req, res);
                 }
-                let data = {
-                    'paths': generatePaths(pathname),
-                    'note': parseNote(filename)
-                };
-                res.end(noteTemplate(data));
+                sendNoteRender(res, pathname, filename);
             });
         } else {
-            return notFound(res);
+            return notFound(req, res);
         }
     }
 };
@@ -151,7 +193,7 @@ const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err);
+        return console.error('something bad happened', err);
     }
     console.log(`server is listening on ${port}`);
 });
