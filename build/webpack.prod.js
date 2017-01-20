8512d7bf18baa98e792bdb6024df4ebb0668eccd
hanxi <hanxi.info@gmail.com>
2017-01-20 11:31:10 +0800

    auto backup

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -32,6 +32,7 @@ base.plugins.push(
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production')
   }),
+  /*
   new webpack.optimize.UglifyJsPlugin({
     sourceMap: true,
     compress: {
@@ -41,6 +42,7 @@ base.plugins.push(
       comments: false
     }
   }),
+  */
   // extract vendor chunks
   new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
