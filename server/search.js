16eceede6f255cf83f19d9b147cff5e49de4b1e0
涵曦 <hanxi.info@gmail.com>
2017-07-20 11:47:00 +0800

    search path

--- server/search.js
+++ server/search.js
@@ -6,7 +6,11 @@ const C = require('./constant')
 module.exports = (text, callback) => {
   findInFiles.findSync({ 'term': text, 'flags': 'ig' }, C.noteDir, '.md$')
         .then((results) => {
-          callback(Object.assign({}, results))
+          let ret = {}
+          for (const key of Object.keys(results)) {
+            ret[key.replace(C.noteDir,"")] = results[key]
+          }
+          callback(ret)
         })
 }
 
